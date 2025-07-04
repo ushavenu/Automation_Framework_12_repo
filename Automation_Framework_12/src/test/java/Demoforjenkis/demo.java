374b8868e2a0ecaaddf6cbe830d09b18b17c14f8
VENUGOBAL <VENUGOBAL@LAPTOP-H4MMLA92>
2025-07-04 13:58:17 +0530

    hiiii added

---
+++ Automation_Framework_12/src/test/java/Demoforjenkis/demo.java
@@ -0,0 +1,13 @@
+package Demoforjenkis;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void running()
+	{
+		Reporter.log("hii",true);
+	}
+}
